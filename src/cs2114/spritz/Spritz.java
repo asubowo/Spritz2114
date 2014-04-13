3b4ab277d9c6c37e378d7f62759645ab26c5a87e
Andrew Subowo <andrew.subowo@gmail.com>
2014-04-13 15:58:18 -0400

    Update commit

---
+++ src/cs2114/spritz/Spritz.java
@@ -0,0 +1,25 @@
+package cs2114.spritz;
+
+/**
+ * This is the logic base for the Spritz application.
+ * 
+ * @author Andrew Subowo (asubowo)
+ * @author 
+ * @author 
+ * @version 4.13.2014
+ *
+ */
+public class Spritz {
+
+	/**
+	 * Constructor for this class
+	 */
+	public Spritz(){
+		
+	}
+	
+	//TODO - We can parse .txt files, so let's place one into the working folder.
+	//From there, we can read the .txt file. Or we can try to parse online files
+	//instead.
+	//Thoughts?
+}
