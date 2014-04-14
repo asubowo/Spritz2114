22a9c6a9b521ddee8e003cf0e1325195b8c8d686
Andrew Subowo <andrew.subowo@gmail.com>
2014-04-14 12:36:35 -0400

    Added parsing ability

--- src/cs2114/spritz/Spritz.java
+++ src/cs2114/spritz/Spritz.java
@@ -19,15 +19,35 @@ public class Spritz {
 
 	private File testFile;
 	private Scanner input;
-	
+	private String token;
+	private CircularLinkedList<String> dataArray;
 	
 	
 	/**
 	 * Constructor for this class
+	 * The default class displays the embedded text file, Lorem Ipsum.
 	 */
 	public Spritz() throws IOException {
 		testFile = new File("\resources\testFile.txt");
 		input = new Scanner(testFile);
+		dataArray = new CircularLinkedList<String>();
+		parseInput();
 	}
+	
+	/**
+	 * Overloaded constructor - able to take in arguments
+	 */
+	//public Spritz(website URL, etc.)
+	
+	/**
+	 * Parses the input
+	 */
+	public void parseInput() {
+		while (input.hasNext()){
+			token = input.next();
+			dataArray.add(token);
+		}
+	}
+	
 	
 }
