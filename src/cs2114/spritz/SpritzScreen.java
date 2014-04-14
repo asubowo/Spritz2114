25b994103a425c33c84396c2aee9cefb2fd66e25
CoryVT <arch518@vt.edu>
2014-04-14 12:34:45 -0400

    Update SpritzScreen.java

--- src/cs2114/spritz/SpritzScreen.java
+++ src/cs2114/spritz/SpritzScreen.java
@@ -1,1 +1,26 @@
 package cs2114.spritz;
+
+import sofia.app.Screen;
+import android.widget.TextView;
+
+/**
+ * This is the screen for the Spritz application.
+ * 
+ * @author Andrew Subowo (asubowo)
+ * @author Cory Howard (arch518)
+ * @author 
+ * @version 4.13.2014
+ *
+ */
+public class SpritzScreen {
+    
+    Spritz spritz;
+    
+    /**
+     * Creates a new blank screen
+     */
+    public void initialize(){
+        spritz = new Spritz();
+        textView1.setText("");
+    }
+}
