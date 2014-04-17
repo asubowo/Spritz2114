1b2b3ccc390431500561cd25cf45647b90737dae
Andrew Subowo <andrew.subowo@gmail.com>
2014-04-16 22:06:08 -0400

    update

--- src/cs2114/spritz/SpritzScreen.java
+++ src/cs2114/spritz/SpritzScreen.java
@@ -1,8 +1,6 @@
 package cs2114.spritz;
 
 import java.io.InputStream;
-
-
 import com.spritz2114.R;
 
 import sofia.app.Screen;
@@ -21,50 +19,50 @@ import sofia.util.Timer;
  */
 public class SpritzScreen extends Screen {
 
-    private Spritz spritz;
-    private TextView spritzDisplay;
-    private Button next;
-    private Button previous;
-    private InputStream is;
-    private Timer timer;
+	private Spritz spritz;
+	private TextView spritzDisplay;
+	private Button next;
+	private Button previous;
+	private InputStream is;
+	private Timer timer;
 
-    /**
-     * Creates a new blank screen
-     */
-    public void initialize() {
+	/**
+	 * Creates a new blank screen
+	 */
+	public void initialize() {
 
-        spritzDisplay.setText("");
-        is = getResources().openRawResource(R.raw.testfile);
-        spritz = new Spritz(is);
-    }
+		spritzDisplay.setText("");
+		is = getResources().openRawResource(R.raw.testfile);
+		spritz = new Spritz(is);
+	}
 
-    /**
-     * The next button
-     */
-    public void nextClicked() {
-        spritzDisplay.setText(spritz.next());
-    }
+	/**
+	 * The next button
+	 */
+	public void nextClicked() {
+		spritzDisplay.setText(spritz.next());
+	}
 
-    /**
-     * The previous button
-     */
-    public void previousClicked(){
-        spritzDisplay.setText(spritz.previous());
-    }
+	/**
+	 * The previous button
+	 */
+	public void previousClicked(){
+		spritzDisplay.setText(spritz.previous());
+	}
 
-    /**
-     * The play button
-     */
-    public void playClicked() {
-        timer = Timer.callRepeatedly(this, "nextClicked", 200);
-    }
+	/**
+	 * The play button
+	 */
+	public void playClicked() {
+		timer = Timer.callRepeatedly(this, "nextClicked", 200);
+	}
 
-    /**
-     * The pause button
-     */
-    public void pauseClicked() {
-        if (timer.isRunning()) {
-            timer.stop();
-        }
-    }
+	/**
+	 * The pause button
+	 */
+	public void pauseClicked() {
+		if (timer.isRunning()) {
+			timer.stop();
+		}
+	}
 }
