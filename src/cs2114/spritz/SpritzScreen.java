29270c5220e3a8333c65e1fc4eb709e9a97545f5
CoryVT <arch518@vt.edu>
2014-04-16 21:04:19 -0400

    Update SpritzScreen.java
    Plays at 300 wpm but the pause button crashes the app

--- src/cs2114/spritz/SpritzScreen.java
+++ src/cs2114/spritz/SpritzScreen.java
@@ -8,62 +8,61 @@ import com.spritz2114.R;
 import sofia.app.Screen;
 import android.widget.Button;
 import android.widget.TextView;
+import sofia.util.Timer;
 
 /**
  * This is the screen for the Spritz application.
- * 
+ *
  * @author Andrew Subowo (asubowo)
  * @author Cory Howard (arch518)
- * @author 
+ * @author
  * @version 4.13.2014
  *
  */
 public class SpritzScreen extends Screen {
-    
+
     private Spritz spritz;
     private TextView spritzDisplay;
     private Button next;
     private Button previous;
     private InputStream is;
-    
+    private Timer timer;
+
     /**
      * Creates a new blank screen
      */
     public void initialize() {
-    	
+
         spritzDisplay.setText("");
-        is = getResources().openRawResource(R.raw.testfile);   
+        is = getResources().openRawResource(R.raw.testfile);
         spritz = new Spritz(is);
     }
-    
+
     /**
      * The next button
      */
     public void nextClicked() {
-    	spritzDisplay.setText(spritz.next());
+        spritzDisplay.setText(spritz.next());
     }
-    
+
     /**
      * The previous button
      */
     public void previousClicked(){
-    	spritzDisplay.setText(spritz.previous());
+        spritzDisplay.setText(spritz.previous());
     }
-    
+
     /**
      * The play button
      */
     public void playClicked() {
-    	spritzDisplay.setText(spritz.getCurrentWord());
-    	next.setEnabled(false);
-    	previous.setEnabled(false);
+        Timer.callRepeatedly(this, "nextClicked", 200);
     }
-    
+
     /**
      * The pause button
      */
     public void pauseClicked() {
-    	next.setEnabled(true);
-    	previous.setEnabled(true);
+        timer.stop();
     }
 }
