be7a84e0096a61d666f1f5cebac02bbe881373b9
Andrew Subowo <andrew.subowo@gmail.com>
2014-04-30 18:18:32 -0400

    Update to test fixtures

--- src/cs2114/spritz/CircularLinkedListTests.java
+++ src/cs2114/spritz/CircularLinkedListTests.java
@@ -1,5 +1,7 @@
 package cs2114.spritz;
 
+import java.util.NoSuchElementException;
+
 import student.TestCase;
 
 /**
@@ -12,40 +14,60 @@ import student.TestCase;
 public class CircularLinkedListTests extends TestCase {
 
 	private CircularLinkedList<String> dataArray;
-	
+
 	/**
 	 * Called before every test case
 	 */
 	public void setUp() {
 		dataArray = new CircularLinkedList<String>();
 	}
-	
+
 	/**
 	 * Tests all aspects of the CircularLinkedList object
 	 */
 	public void testCircularLinkedList() {
 		assertEquals(0, dataArray.size());
-		
+
 		dataArray.add("hello");
 		dataArray.add("world!");
-		
+
 		dataArray.next();
 		assertEquals("hello", dataArray.getCurrent());
 		dataArray.previous();
 		assertEquals("world!", dataArray.getCurrent());
-		
-		
+		assertTrue(dataArray.end());
+
+
 		dataArray.add("this");
 		dataArray.add("is");
 		dataArray.add("bob");
-	
+
 		assertEquals("bob", dataArray.getCurrent());
 		dataArray.clear();
-		
+		dataArray.reset();
+
 		dataArray.add("hello");
 		assertEquals(1, dataArray.size());
 		assertEquals("hello", dataArray.getCurrent());
+
 	}
-	
-	
+
+	/**
+	 * Tests get current of an empty list
+	 */
+	public void testEmpty() {
+		Exception thrown = null;
+
+		try {
+			dataArray.getCurrent();
+		}
+
+		catch (Exception e) {
+			thrown = e;
+		}
+
+		assertTrue(thrown instanceof NoSuchElementException);
+		assertEquals("Cannot get current. The list is empty!",
+				thrown.getMessage());
+	}
 }
